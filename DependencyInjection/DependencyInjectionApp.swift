ddd4f63bab70e112be7a1e90b924b9f09c1eeeea
Rushikesh Suradhkar <Rushikesh.Suradhkar@Amway.com>
2025-04-04 13:52:42 +0530

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Rushikesh Suradkar on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
